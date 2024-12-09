415fc39c8c788507ca3d602e3a93b599f3a2e4cf
Calu12 <coemgen.genevey@gmail.com>
2024-12-09 14:53:56 +0100

    debut localStorage

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,14 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: '/announce/:id',
+        component: () => import('pages/AnnonceDetailsPage.vue'), // La page de détail de l'annonce
+        props: true, // Passe l'ID comme prop à la page
+      },
+    ],
   },
 
   // Always leave this as last one,
